1ddaa11f693d131e12eb0b872473ad094f9c9ef8
rishab-sharma <rishusharma.sharma7@gmail.com>
2020-12-04 13:35:56 +0530

    Initail Commit

---
+++ docs/js/main.js
@@ -0,0 +1,11 @@
+(function($) {
+    "use strict";
+  
+    var nav = $('nav');
+    var navHeight = nav.outerHeight();
+  
+    $('.navbar-toggler').on('click', function() {
+      if (!$('#mainNav').hasClass('navbar-reduce')) {
+        $('#mainNav').addClass('navbar-reduce');
+      }
+    })
