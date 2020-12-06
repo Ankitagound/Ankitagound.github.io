f1dc2e1eab3fbb1f12a5488870e355bffae262bc
Ankita Gound <37178418+Ankitagound@users.noreply.github.com>
2020-12-06 14:29:36 +0530

    Update contact.php

--- docs/Contact/forms/contact.php
+++ docs/Contact/forms/contact.php
@@ -50,7 +50,7 @@ if (isset($_POST['send-msg']))
       $mail->Username   = 'designingmind.ux@gmail.com';                     // SMTP username
       $mail->Password   = 'Ankita@123';                               // SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-      $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+      $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
   
       //Recipients
       $mail->setFrom('designingmind.ux@gmail.com', 'Query from USer');
